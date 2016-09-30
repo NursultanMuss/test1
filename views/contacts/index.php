21a560fe4bfbdbdddb675f735fd678feb4d6d7c6
Nursultan Mussabayev <nursalga@gmail.com>
2016-09-30 17:55:39 +0600

    30.09.2016

--- views/contacts/index.php
+++ views/contacts/index.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="/template/css/style1.css">
     <link rel="stylesheet" href="/template/css/media.css">
 </head>
-<body class="wrapper" style="background: url('/template/images/Contacts.jpg') repeat fixed 50% 50%">
+<body class="wrapper contacts">
 <?php include ROOT.'/views/layout/sidebar.php'?>
 
 <div class="page-content-wrapper">
@@ -23,28 +23,23 @@
             <div class="contacts_page">
                 <h1>Контакты</h1>
                 <div class="menuBottom">
-                    <div class="address" style="position:relative; text-align: center;">
-                        <img src="/template/images/place_tag.png" alt="metka" class="img-responsive" style="display:inline-block; margin-right: 5px;">
-                        <p style="font-size:18px;">Казахстан, г.Шымкент, ул.Рыскулова 8а, Бизнес центр Даму</p>
+                    <div class="address">
+                        <img src="/template/images/place_tag.png" alt="metka" class="img-responsive">
+                        <p>Казахстан, г.Шымкент, ул.Рыскулова 8а, Бизнес центр Даму</p>
                     </div>
-                    <div class="telContacts" style="position: relative; text-align: center;">
-                        <div class="phoneLogo" style="">
-                            <img src="/template/images/phone.png" alt="phone" class="img-responsive">
-                        </div>
-                        <p class="content" style="font-size: 23px; color: #000000; font-weight: 800;">
+                    <div class="telContacts">
+                        <img src="/template/images/phone.png" alt="phone" class="img-responsive">
+                        <p class="content">
                             +7 747 108 07 25<br>
                             +7 747 717 22 06<br>
                             +7 7252 97 57 90<br>
-
                         </p>
                     </div>
-                    <div class="emailAddres" style="position: relative; text-align: center;">
-                        <div class="emailLogo" style="margin-right: 10px;">
-                            <img src="/template/images/mailbox.png" alt="email" class="img-responsive">
-                        </div>
-                        <p class="content" style="color:#000000; font-size: 18px"> ok@greengo.kz</p>
+                    <div class="emailAddres">
+                        <img src="/template/images/mailbox.png" alt="email" class="img-responsive">
+                        <p class="content"> ok@greengo.kz</p>
                     </div>
-                    <div class="social-links" style="text-align:center;">
+                    <div class="social-links">
                         <div class="facebookLogo">
                             <a href="#"><img src="/template/images/facebook_logo.png" alt="facebook" class="img-responsive"></a>
                         </div>
@@ -63,16 +58,16 @@
 </div>
 <div class="look_at_the_map">
     <img src="/template/images/map.png" alt="map" class="img-responsive center-block">
-    <p style="display:block; text-align: center; font: 800 19px Verdana; color: #ff2d2d; text-transform: uppercase;">Просмотреть на карте</p>
+    <p>Просмотреть на карте</p>
     <a href="">
         <img src="/template/images/strelka_black.png" alt="black_strelka" class="img-responsive center-block">
     </a>
 </div>
 <div class="zayavka">
-    <div class="comment" style="margin-top: -120px;">
+    <div class="comment">
         <img src="/template/images/comment_logo.png" alt="comment" class="img-responsive">
     </div>
-    <div class="right_slide" style="margin-top:-10px;">
+    <div class="right_slide">
         <p>Оставить заявку</p>
     </div>
 </div>
@@ -81,15 +76,15 @@
         <img src="/template/images/comment_logo_asd.png" alt="comment" class="img-responsive center-block">
     </div>
     <p>Ваша заявка</p>
-    <form name="zayavka" method="post" action="">
-        <div class="input-group" style="padding: 0px 35px 25px 25px;">
+    <form name="zayavka" method="get" action="">
+        <div class="input-group">
             <input type="text" class="form-control" value="Ваше имя" name="Name" onfocus="if (this.value == 'Ваше имя') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Ваше имя';}">
             <input type="text" class="form-control" value="Эл. почта" name="email" onfocus="if (this.value == 'Эл. почта') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Эл. почта';}">
             <input type="text" class="form-control" value="Телефон" name="phone Number" onfocus="if (this.value == 'Телефон') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Телефон';}">
             <textarea rows="6" cols="20" placeholder="Текст сообщения" type="text" class="form-control" name="Text">
                 </textarea>
         </div>
-        <input type="submit" class="btn btn-default center-block" id="submit button" value="отправить заявку" style="background: #961f1f; text-transform: uppercase; font: 35px condensbold; color: #ffffff;">
+        <input type="submit" class="otpravka btn btn-default center-block" id="submit button" value="отправить заявку">
     </form>
 
 </div>
